8154c805d97f7a84e7f33ec1684e34455fc6c85d
axcd <maoyuyin@126.com>
2019-06-04 23:11:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 
   var transition = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"];
 
-  $("[data-role='content']").click(function(){
+  $("#pagetwo").click(function(){
 
     $("[href='#pageone']").removeAttr(data-transition);
     $("[href='#pageone']").prop("data-transition",transition[Math.floor(Math.random()*10)]);
 
   });
 
-$("[data-role='content']").click(function(){
+$("#pageone").click(function(){
 
     $("[href='#pagetwo']").removeAttr(data-transition);
     $("[href='#pagetwo']").prop("data-transition",transition[Math.floor(Math.random()*10)]);
