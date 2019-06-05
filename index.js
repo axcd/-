1060f11bda7c4de201c3301da1e150727c497ab3
axcd <maoyuyin@126.com>
2019-06-05 12:27:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ $(document).ready(function(){
 
   var transition = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"];
 
-  $("div#pagetwo").click(function(){
+  //$("div#pagetwo").click(function(){
 
-    //$("[href='#pageone']").css("data-transition",transition[Math.floor(Math.random()*10)]);
-    $("[href='#pagetwo']").attr("data-transition",transition[Math.floor(Math.random()*10)]);
+    //$("[href='#pagetwo']").attr("data-transition",transition[Math.floor(Math.random()*10)]);
     //$("[href='#pagetwo']").removeAttr("data-transition");
 
-  });
+  //});
 
-$("div#pageone").click(function(){
+//$("div#pageone").click(function(){
 
-    $("[href='#pageone']").attr("data-transition",transition[Math.floor(Math.random()*10)]);
+    //$("[href='#pageone']").attr("data-transition",transition[Math.floor(Math.random()*10)]);
     //$("[href='#pageone']").removeAttr("data-transition");
+
+  //});
+  
+  $("a").click(function(){
+  
+    $("a").css("herf","#"+ transition[Math.floor(Math.random()*10)]);
 
   });
 
