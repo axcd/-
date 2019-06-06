35c1e5da67ed10ad9dc6f43aa177ed0c76a07147
axcd <maoyuyin@126.com>
2019-06-06 23:17:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 $(document).ready(function(){
 
-  var t = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"];
-  
+  var t = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"]; 
   var p = $("p");
-  alert(p[0]);
-  for(i = 0;i < p.length;i++){
-    alert(i);
-    p.click(function(){
-      alert(p[i]);
-    //var a = this.next();
-    var x = t[Math.floor(Math.random()*10)];
-    //this.setAttribute("data-transition",x);
-    //alert(this.getAttribute("data-transition"));
-    //a.href = "#"+x;
-    //alert(a);
-    //alert(a.href);
-  });
 
-  };
+  //for(i = 0;i < p.length;i++){
+    
+    p.click(function(){
+      alert(this);
+      //var a = this.next();
+      var x = t[Math.floor(Math.random()*10)];
+      //this.setAttribute("data-transition",x);
+      //alert(this.getAttribute("data-transition"));
+      //a.href = "#"+x;
+      //alert(a);
+      //alert(a.href);
+    });
+
+  //};
 
 });
 
