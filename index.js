1f33b992a4cdd16207ed612156b2e060d6660a71
axcd <maoyuyin@126.com>
2019-06-06 14:08:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 $(document).ready(function(){
 
   var transition = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"];
-
-  //$("div#pagetwo").click(function(){
-
-    //$("[href='#pagetwo']").attr("data-transition",transition[Math.floor(Math.random()*10)]);
-    //$("[href='#pagetwo']").removeAttr("data-transition");
-
-  //});
-
-//$("div#pageone").click(function(){
-
-    //$("[href='#pageone']").attr("data-transition",transition[Math.floor(Math.random()*10)]);
-    //$("[href='#pageone']").removeAttr("data-transition");
-
-  //});
   
   $("a").click(function(){
     x = transition[Math.floor(Math.random()*10)];
-    //window.alert(x);
-    $(this).getAttributeNode("style").value="herf:#" + x ;
     $(this).setAttribute("herf","#" + x);
   });
-
 
 });
 
