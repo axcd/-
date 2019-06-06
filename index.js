1e42798e4458a431ce59af8679cd62d3fc695dd5
axcd <maoyuyin@126.com>
2019-06-06 14:51:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     x = transition[Math.floor(Math.random()*10)];
     alert(x);
     //$(this).setAttribute("href","#" + x);
-    $(this).find("p")[0].href = "#"+x;
+    $(this).find("a")[0].href = "#"+x;
     alert($(this).href);
   });
 
