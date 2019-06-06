b264f8b0a59e282f0a99c500e87e085ba9bb657f
axcd <maoyuyin@126.com>
2019-06-06 22:25:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ $(document).ready(function(){
   var p = $("p");
   //alert(p);
   for(var i = 0;i < p.length;i++){
-    alert(123);
+    //alert(123);
     p[i].mouseover(function(){
     alert(p[i]);
-    var a = this.next();
+    //var a = this.next();
     var x = t[Math.floor(Math.random()*10)];
     //this.setAttribute("data-transition",x);
     //alert(this.getAttribute("data-transition"));
-    a.href = "#"+x;
-    alert(a);
-    alert(a.href);
+    //a.href = "#"+x;
+    //alert(a);
+    //alert(a.href);
   });
 
   };
