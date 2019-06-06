307667ee6dd52d61df05b99ac6037f8bf06bdccc
axcd <maoyuyin@126.com>
2019-06-06 14:22:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   var transition = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"];
   
   $("a").click(function(){
+    alert(this);
     x = transition[Math.floor(Math.random()*10)];
     alert(x);
     $(this).setAttribute("href","#" + x);
