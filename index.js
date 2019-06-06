057dd34aa63c6eba981e83b30e676d0128b33523
axcd <maoyuyin@126.com>
2019-06-06 16:02:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   var t = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"];
 
   $("p").click(function(){
+    alert(this);
     var a = this.next();
     var x = t[Math.floor(Math.random()*10)];
     //this.setAttribute("data-transition",x);
