ca9457af8e5a64d7a8086fac90186991509450da
axcd <maoyuyin@126.com>
2019-06-07 00:11:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   var t = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"]; 
     
-  $("#fade").click(function(){
+  $("a").click(function(){
     alert(this);
     //var a = this.find("a");
     var x = t[Math.floor(Math.random()*10)];
