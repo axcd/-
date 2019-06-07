7afc1b464b51a86836a96cc83d0a805c43b0d7f1
axcd <maoyuyin@126.com>
2019-06-07 01:45:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
 
   var t = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn", "none"]; 
-    /*
+    
     $("a").click(function(){
     var x = t[Math.floor(Math.random()*9)];
     alert(x);
     this.href = "#"+x;
   });
-*/
-  
+
+  /*
   $("a").click(function(){
     var x = t[Math.floor(Math.random()*9)];
     alert(x+"1");
     this.setAttribute("data-transition",x);
     //alert(this.getAttribute("data-transition"));
   });
+*/
 
 });
 
