9f765c141b38016b1d7dd40302c1b414e352c108
axcd <maoyuyin@126.com>
2019-06-08 20:57:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
 
   var t = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn"];
   var x = null;
+
   $("a").click(function(){
     x = t[Math.floor(Math.random()*9)];
     this.setAttribute("data-transitio",x);  
     alert(x);
   });
 
-$("div").click(function(){
+  $("p").click(function(){
     x = t[Math.floor(Math.random()*9)];
     this.children("a").setAttribute("data-transition",x);alert(1);
     window.location.href="https://axcd.github.io/mao/"+this.children("a").href;
