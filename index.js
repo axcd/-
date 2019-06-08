0ab1a0a80f63c10f941afcab9a2cf921a429cbee
axcd <maoyuyin@126.com>
2019-06-08 08:45:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
 
   var t = ["fade", "flip", "flow", "pop", "slide", "slidefade", "slideup", "slidedown", "turn"];
-  var x;
 
   $("a").click(function(){
-
-    x = null;
-    x = t[Math.floor(Math.random()*9)];
+    var x = t[Math.floor(Math.random()*9)];
     this.setAttribute("data-transition",x);  
+    x = null;
   });
 
 });
