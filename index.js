8ffe03171909af1879d73d252c44b9bf484e44c8
axcd <maoyuyin@126.com>
2019-06-09 00:32:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
   $("p").click(function(){
     x = t[Math.floor(Math.random()*9)]; alert(this);
-    var a = this.lastChild("a"); alert(2);
+    var a = this.lastChild("a"); alert(a);
     a.setAttribute("data-transition",x); alert(2);
     window.location.href="https://www.baidu.com"; //axcd.github.io/mao/"+this.find("a").href;
   });
